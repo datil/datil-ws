1c3cfe21a974a3bfbd1a3c173538f7901c34f1ca
Alejandro Vinueza <alejandrovinuezap@gmail.com>
2014-12-19 16:43:50 -0500

    web auth for dev

--- es/Gruntfile.js
+++ es/Gruntfile.js
@@ -239,6 +239,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'fonts/{,*/}*.*',
             '.htaccess',
+            'private/.htpasswd',
             'images/{,*/}*.{webp,gif}'
           ]
         }]
